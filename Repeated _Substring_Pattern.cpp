38b0922140611f31659882d5edbcd8c61150e4b9
sagarsanidhya <sagarsanidhya@gmail.com>
2024-06-01 23:33:48 +0530

    recommit

--- Repeated _Substring_Pattern.cpp
+++ Repeated _Substring_Pattern.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
 
-    // Your code here
-    class Solution {
+ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         // Concatenate s with itself
@@ -20,6 +18,27 @@ public:
         //If the result is npos, it means the substring s was not found in modified_ss.
     }
 };
+
+
+int main() {
+
+    // Your code here
+   Solution solution;
+    // Test cases
+    string s1 = "abab";
+    string s2 = "aba";
+    string s3 = "abcabcabcabc";
+
+    // Check the results
+    bool result1 = solution.repeatedSubstringPattern(s1); // Expected: true
+    bool result2 = solution.repeatedSubstringPattern(s2); // Expected: false
+    bool result3 = solution.repeatedSubstringPattern(s3); // Expected: true
+
+    // Output results
+    cout << boolalpha; // Print true/false instead of 1/0
+    cout << "s1: " << result1 << endl; // true
+    cout << "s2: " << result2 << endl; // false
+    cout << "s3: " << result3 << endl; // true
 
 
     return 0;
